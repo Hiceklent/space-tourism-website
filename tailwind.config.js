bc3fbe033e8588580c249b24e44ad7f212a0b079
Holguer Pinzon <93726097+Hiceklent@users.noreply.github.com>
2024-08-17 15:00:26 -0500

    💄 [ADD] Design System Anadido

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ['Bellefair', 'serif'], // Para headings
+        body: ['Barlow', 'sans-serif'], // Para body text
+        nav: ['Barlow Condensed', 'sans-serif'], // Para texto de navegación
+      },
+
+      fontSize: {
+        'h1': '150px',
+        'h2': '100px',
+        'h3': '56px',
+        'h4': '32px',
+        'h5': '28px',
+        'subheading1': '28px',
+        'subheading2': '14px',
+        'nav': '16px',
+        'baseSize': '16px',
+      },
+      letterSpacing: {
+        'nav': '2.7px',
+        'h5': '4.75px',
+        'subheading2': '2.35px',
+      },
+      colors: {
+        primary: '#0B0D17',
+        secondary: '#D0D6F9',
+        white: '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 }
