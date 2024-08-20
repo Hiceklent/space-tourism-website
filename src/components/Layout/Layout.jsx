3b462ffd5063eaca4eabf7ac92088be73c06084a
Holguer Pinzon <93726097+Hiceklent@users.noreply.github.com>
2024-08-19 21:12:22 -0500

    🐛 [FIX] Fix img reoutes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,12 @@
-
 import Header from "../common/Header/Header"
-import Footer from "../common/Footer/Footer"
 import { Outlet } from "react-router-dom"
 const Layout = () => {
     return (
         <>
-
             <Header />
             <main>
                 <Outlet />
             </main>
-            <Footer />
         </>
     )
 }
